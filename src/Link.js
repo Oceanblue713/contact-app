8e69a1bc490a07fc6f2fa5f354edcdc6a5fd3b6e
Anri Liy <anri@realquantum.com>
2019-07-23 06:50:26 -0500

    Fix Link and Css

--- src/Link.js
+++ src/Link.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
 import  Figure  from 'react-bootstrap/Figure';
-import Col from 'react-bootstrap/Col';
 
 class Link extends Component {
     render() {
         return (       
             <div className="link">
-                <div className="link-board">
-                    <h2>リンク</h2>
-                    <Col xs={12} md={6}>
-                        <a href="http://www.okisi.org/">  
-                            <Figure.Image 
-                            width={171}
-                            height={180}
-                            src={require('./toothbrush.jpg')} roundedCircle 
-                            />
-                        </a>
-                    </Col>
-                    <Col xs={12} md={6}>
-                            <Figure.Image 
-                            width={171}
-                            height={180}
-                            src={require('./fish.jpg')} roundedCircle />  
-                    </Col>                          
-                </div>
+                <h2>リンク</h2>      
+                    <a href="http://www.okisi.org/">  
+                        <Figure.Image 
+                        width={171}
+                        height={180}
+                        src={require('./toothbrush.jpg')} roundedCircle 
+                        />
+                    </a>
+                        <Figure.Image 
+                        width={171}
+                        height={180}
+                        src={require('./fish.jpg')} roundedCircle />                         
             </div>
         )
     }
